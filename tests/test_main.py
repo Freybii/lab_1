b09b415d19efc237742fb346542bff7de8688108
Freybii <92971226+Freybii@users.noreply.github.com>
2025-04-07 04:10:17 -0700

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from main import Developer, Project
+from datetime import datetime
+
+def test_assign_developer_to_project():
+    dev = Developer(1, "Test Dev", "123 Street", "123456", "test@example.com", 1, 1000)
+    project = Project("Test Project", [], 1, datetime.now())
+
+    project.add_developer(dev)
+
+    assert dev in project.developers
+    assert project in dev.assign_projects()
